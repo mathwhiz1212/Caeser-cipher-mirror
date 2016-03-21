3705bde9837831cbf711b6e66cc8db46c635d89e
Joshua Mudge <mathwhiz1212@users.noreply.github.com>
2016-03-21 13:29:04 -0600

    Two letter input. Renamed variables and added more comments.

--- helper.js
+++ helper.js
@@ -1,18 +1,19 @@
 "use strict";
 //Licensed under the Apache License 2.0. See license file for information.
-
+//Need to get the length of the string, then converter the string to an array with each memeber of the array being a character. Then we need to reference each letter in the array and encipher them using the encipher()
+//  firstNumSplit[0] and string.length and split
 function helper() {
 
   var firstNum = "initial";
-  var SecondNum = "initial";
-  var Answer = "initial";
+  var shift = "initial";
+  var firstLetter = "initial";
   var letters = /^[A-Za-z]+$/;
-
-  function getinput() {
-
+  
+    //Get input
     var firstNum = prompt("Please enter the letter of the alphabet.");
-
+    
     //Checks if input is a letter. Uses a modified version of code found here: http://www.w3resource.com/javascript/form/all-letters-field.php
+    
     if (firstNum.match(letters))
     {
       //Do nothing.
@@ -21,179 +22,361 @@ function helper() {
     {
       alert("Please enter only letters.");
     }
+    
+    //Get length of input.
+    var inputLength = firstNum.length;
+    //alert(inputLength);
+    
+    var firstNumSplit = firstNum.split(""); 
+    //alert(firstNumSplit[0]);
+    //alert(firstNumSplit[1]);
+    
+    var shift = Number(prompt("Please enter the shift."));
 
-    var SecondNum = Number(prompt("Please enter the shift."));
-  }
-
-  function encipher() {
-
-    if (firstNum === "a") {
+  //Encipher
+  
+  //If there is one or more letters in the string then encipher the first letter.
+  
+  if (inputLength => 1) {
+    
+    if (firstNumSplit[0] === "a") {
       var firstNum = 1;
     }
-    if (firstNum === "b") {
+    if (firstNumSplit[0] === "b") {
       var firstNum = "2";
     }
-    if (firstNum === "c") {
+    if (firstNumSplit[0] === "c") {
       var firstNum = "3";
     }
-    if (firstNum === "d") {
+    if (firstNumSplit[0] === "d") {
       var firstNum = "4";
     }
-    if (firstNum === "e") {
+    if (firstNumSplit[0] === "e") {
       var firstNum = "5";
     }
-    if (firstNum === "f") {
+    if (firstNumSplit[0] === "f") {
       var firstNum = "6";
     }
-    if (firstNum === "g") {
+    if (firstNumSplit[0] === "g") {
       var firstNum = "7";
     }
-    if (firstNum === "h") {
+    if (firstNumSplit[0] === "h") {
       var firstNum = "8";
     }
-    if (firstNum === "i") {
+    if (firstNumSplit[0] === "i") {
       var firstNum = "9";
     }
-    if (firstNum === "j") {
+    if (firstNumSplit[0] === "j") {
       var firstNum = "10";
     }
-    if (firstNum === "k") {
+    if (firstNumSplit[0] === "k") {
       var firstNum = "11";
     }
-    if (firstNum === "l") {
+    if (firstNumSplit[0] === "l") {
       var firstNum = "12";
     }
-    if (firstNum === "m") {
+    if (firstNumSplit[0] === "m") {
       var firstNum = "13";
     }
-    if (firstNum === "n") {
+    if (firstNumSplit[0] === "n") {
       var firstNum = "14";
     }
-    if (firstNum === "o") {
+    if (firstNumSplit[0] === "o") {
       var firstNum = "15";
     }
-    if (firstNum === "p") {
+    if (firstNumSplit[0] === "p") {
       var firstNum = "16";
     }
-    if (firstNum === "q") {
+    if (firstNumSplit[0] === "q") {
       var firstNum = "17";
     }
-    if (firstNum === "r") {
+    if (firstNumSplit[0] === "r") {
       var firstNum = "18";
     }
-    if (firstNum === "s") {
+    if (firstNumSplit[0] === "s") {
       var firstNum = "19";
     }
-    if (firstNum === "t") {
+    if (firstNumSplit[0] === "t") {
       var firstNum = "20";
     }
-    if (firstNum === "u") {
+    if (firstNumSplit[0] === "u") {
       var firstNum = "21";
     }
-    if (firstNum === "v") {
+    if (firstNumSplit[0] === "v") {
       var firstNum = "22";
     }
-    if (firstNum === "w") {
+    if (firstNumSplit[0] === "w") {
       var firstNum = "23";
     }
-    if (firstNum === "x") {
+    if (firstNumSplit[0] === "x") {
       var firstNum = "24";
     }
-    if (firstNum === "y") {
+    if (firstNumSplit[0] === "y") {
       var firstNum = "25";
     }
-    if (firstNum === "z") {
+    if (firstNumSplit[0] === "z") {
       var firstNum = "26";
     }
 
-    var Answer = Number(firstNum) + SecondNum;
-    if (Answer > 26) {
-      var Answer = Answer - 26;
+    var firstLetter = Number(firstNum) + shift;
+    if (firstLetter > 26) {
+      var firstLetter = firstLetter - 26;
     }
 
-    if (Answer === 1) {
-      var Answer = "a";
+    if (firstLetter === 1) {
+      var firstLetter = "a";
     }
-    if (Answer === 2) {
-      var Answer = "b";
+    if (firstLetter === 2) {
+      var firstLetter = "b";
     }
-    if (Answer === 3) {
-      var Answer = "c";
+    if (firstLetter === 3) {
+      var firstLetter = "c";
     }
-    if (Answer === 4) {
-      var Answer = "d";
+    if (firstLetter === 4) {
+      var firstLetter = "d";
     }
-    if (Answer === 5) {
-      var Answer = "e";
+    if (firstLetter === 5) {
+      var firstLetter = "e";
     }
-    if (Answer === 6) {
-      var Answer = "f";
+    if (firstLetter === 6) {
+      var firstLetter = "f";
     }
-    if (Answer === 7) {
-      var Answer = "g";
+    if (firstLetter === 7) {
+      var firstLetter = "g";
     }
-    if (Answer === 8) {
-      var Answer = "h";
+    if (firstLetter === 8) {
+      var firstLetter = "h";
     }
-    if (Answer === 9) {
-      var Answer = "i";
+    if (firstLetter === 9) {
+      var firstLetter = "i";
     }
-    if (Answer === 10) {
-      var Answer = "j";
+    if (firstLetter === 10) {
+      var firstLetter = "j";
     }
-    if (Answer === 11) {
-      var Answer = "k";
+    if (firstLetter === 11) {
+      var firstLetter = "k";
     }
-    if (Answer === 12) {
-      var Answer = "l";
+    if (firstLetter === 12) {
+      var firstLetter = "l";
     }
-    if (Answer === 13) {
-      var Answer = "m";
+    if (firstLetter === 13) {
+      var firstLetter = "m";
     }
-    if (Answer === 14) {
-      var Answer = "n";
+    if (firstLetter === 14) {
+      var firstLetter = "n";
     }
-    if (Answer === 15) {
-      var Answer = "o";
+    if (firstLetter === 15) {
+      var firstLetter = "o";
     }
-    if (Answer === 16) {
-      var Answer = "p";
+    if (firstLetter === 16) {
+      var firstLetter = "p";
     }
-    if (Answer === 17) {
-      var Answer = "q";
+    if (firstLetter === 17) {
+      var firstLetter = "q";
     }
-    if (Answer === 18) {
-      var Answer = "r";
+    if (firstLetter === 18) {
+      var firstLetter = "r";
     }
-    if (Answer === 19) {
-      var Answer = "s";
+    if (firstLetter === 19) {
+      var firstLetter = "s";
     }
-    if (Answer === 20) {
-      var Answer = "t";
+    if (firstLetter === 20) {
+      var firstLetter = "t";
     }
-    if (Answer === 21) {
-      var Answer = "u";
+    if (firstLetter === 21) {
+      var firstLetter = "u";
     }
-    if (Answer === 22) {
-      var Answer = "v";
+    if (firstLetter === 22) {
+      var firstLetter = "v";
     }
-    if (Answer === 23) {
-      var Answer = "w";
+    if (firstLetter === 23) {
+      var firstLetter = "w";
     }
-    if (Answer === 24) {
-      var Answer = "x";
+    if (firstLetter === 24) {
+      var firstLetter = "x";
     }
-    if (Answer === 25) {
-      var Answer = "y";
+    if (firstLetter === 25) {
+      var firstLetter = "y";
     }
-    if (Answer === 26) {
-      var Answer = "z";
+    if (firstLetter === 26) {
+      var firstLetter = "z";
     }
-  }
+}
+    //If there is two or more letters in the string then encipher the second letter.
+    
+    if (inputLength => 2) {
+      
+      if (firstNumSplit[1] === "a") {
+        var firstNum = 1;
+      }
+      if (firstNumSplit[1] === "b") {
+        var firstNum = "2";
+      }
+      if (firstNumSplit[1] === "c") {
+        var firstNum = "3";
+      }
+      if (firstNumSplit[1] === "d") {
+        var firstNum = "4";
+      }
+      if (firstNumSplit[1] === "e") {
+        var firstNum = "5";
+      }
+      if (firstNumSplit[1] === "f") {
+        var firstNum = "6";
+      }
+      if (firstNumSplit[1] === "g") {
+        var firstNum = "7";
+      }
+      if (firstNumSplit[1] === "h") {
+        var firstNum = "8";
+      }
+      if (firstNumSplit[1] === "i") {
+        var firstNum = "9";
+      }
+      if (firstNumSplit[1] === "j") {
+        var firstNum = "10";
+      }
+      if (firstNumSplit[1] === "k") {
+        var firstNum = "11";
+      }
+      if (firstNumSplit[1] === "l") {
+        var firstNum = "12";
+      }
+      if (firstNumSplit[1] === "m") {
+        var firstNum = "13";
+      }
+      if (firstNumSplit[1] === "n") {
+        var firstNum = "14";
+      }
+      if (firstNumSplit[1] === "o") {
+        var firstNum = "15";
+      }
+      if (firstNumSplit[1] === "p") {
+        var firstNum = "16";
+      }
+      if (firstNumSplit[1] === "q") {
+        var firstNum = "17";
+      }
+      if (firstNumSplit[1] === "r") {
+        var firstNum = "18";
+      }
+      if (firstNumSplit[1] === "s") {
+        var firstNum = "19";
+      }
+      if (firstNumSplit[1] === "t") {
+        var firstNum = "20";
+      }
+      if (firstNumSplit[1] === "u") {
+        var firstNum = "21";
+      }
+      if (firstNumSplit[1] === "v") {
+        var firstNum = "22";
+      }
+      if (firstNumSplit[1] === "w") {
+        var firstNum = "23";
+      }
+      if (firstNumSplit[1] === "x") {
+        var firstNum = "24";
+      }
+      if (firstNumSplit[1] === "y") {
+        var firstNum = "25";
+      }
+      if (firstNumSplit[1] === "z") {
+        var firstNum = "26";
+      }
 
-  function showanswer() {
+      var secondLetter = Number(firstNum) + shift;
+      if (secondLetter > 26) {
+        var secondLetter = secondLetter - 26;
+      }
 
-    alert("Your letter is " +  Answer);
-    document.getElementById("Answer").innerHTML = "Your letter is " + Answer;
-  }
+      if (secondLetter === 1) {
+        var secondLetter = "a";
+      }
+      if (secondLetter === 2) {
+        var secondLetter = "b";
+      }
+      if (secondLetter === 3) {
+        var secondLetter = "c";
+      }
+      if (secondLetter === 4) {
+        var secondLetter = "d";
+      }
+      if (secondLetter === 5) {
+        var secondLetter = "e";
+      }
+      if (secondLetter === 6) {
+        var secondLetter = "f";
+      }
+      if (secondLetter === 7) {
+        var secondLetter = "g";
+      }
+      if (secondLetter === 8) {
+        var secondLetter = "h";
+      }
+      if (secondLetter === 9) {
+        var secondLetter = "i";
+      }
+      if (secondLetter === 10) {
+        var secondLetter = "j";
+      }
+      if (secondLetter === 11) {
+        var secondLetter = "k";
+      }
+      if (secondLetter === 12) {
+        var secondLetter = "l";
+      }
+      if (secondLetter === 13) {
+        var secondLetter = "m";
+      }
+      if (secondLetter === 14) {
+        var secondLetter = "n";
+      }
+      if (secondLetter === 15) {
+        var secondLetter = "o";
+      }
+      if (secondLetter === 16) {
+        var secondLetter = "p";
+      }
+      if (secondLetter === 17) {
+        var secondLetter = "q";
+      }
+      if (secondLetter === 18) {
+        var secondLetter = "r";
+      }
+      if (secondLetter === 19) {
+        var secondLetter = "s";
+      }
+      if (secondLetter === 20) {
+        var secondLetter = "t";
+      }
+      if (secondLetter === 21) {
+        var secondLetter = "u";
+      }
+      if (secondLetter === 22) {
+        var secondLetter = "v";
+      }
+      if (secondLetter === 23) {
+        var secondLetter = "w";
+      }
+      if (secondLetter === 24) {
+        var secondLetter = "x";
+      }
+      if (secondLetter === 25) {
+        var secondLetter = "y";
+      }
+      if (secondLetter === 26) {
+        var secondLetter = "z";
+      }
+}
+
+
+  
+    //Show result
+ 
+    //Combine  letters
+    var Answer = firstLetter + secondLetter;
+     
+    alert("Your Answer is " +  Answer);
+    document.getElementById("Answer").innerHTML = "Your Answer is " + Answer;
 }
