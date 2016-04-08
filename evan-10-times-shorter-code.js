13b5f9ac3018efc50fe09fc05d4be28905b93f86
Joshua Mudge <mathwhiz1212@users.noreply.github.com>
2016-04-07 21:49:13 -0600

    Added Evan's idea for shortening code.

---
+++ evan-10-times-shorter-code.js
@@ -0,0 +1,19 @@
+//for loop
+
+arr = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+
+var shiftNum = 2;
+
+var result = shiftNum + userInput
+
+if (result > 26) {
+var result = result - 26;
+}
+
+arr[result];
+
+//shift the position in the array using the shift input.
+
+//Evan's idea:
+
+"You know how you have all those if tests to determine whether the latter is a,b,etc and then you have another giant set of tests? You could do all of that with on array that had all of the letters in it. What you would want to do would be to loop through the array until you found the letter you have and then add whatever your shift value is to that position. Then at that position, you have the new letter to use"
