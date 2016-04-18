70b715bec415763a8c1c65a611022873f5096738
Joshua Mudge <mathwhiz1212@users.noreply.github.com>
2016-04-18 13:19:13 -0600

    Better spacing and unneeded commented code was removed.

--- encipher.js
+++ encipher.js
@@ -15,14 +15,14 @@ function encipher() {
   var inputLength = "";
   var inputSplit = "";
   var i = "0";
-
+  
   //Get input
-
+  
   var userInput = document.getElementById("userInput").value;
   var shiftNum = document.getElementById("shiftNum").value;
-
-//Checks if input is a letter. Uses a modified version of code found here: http://www.w3resource.com/javascript/form/all-letters-field.php
-
+  
+  //Checks if input is a letter. Uses a modified version of code found here: http://www.w3resource.com/javascript/form/all-letters-field.php
+  
   if (userInput.match(LetterReg))
   {
     //Do nothing.
@@ -30,59 +30,57 @@ function encipher() {
   else
   {
     //Tells them to enter only letters with no caps, spaces or punctuation.
-
+    
     alert("Please enter only letters, no caps, spaces or punctuation in message field.");
   }
-
-if (shiftNum.match(numReg))
+  
+  if (shiftNum.match(numReg))
   {
     //Do nothing.
   }
   else
   {
     //Tells them to enter only numbers.
-
+    
     alert("Please enter only numbers in the shift field.");
   }
-
+  
   //Gets input length.
-
+  
   var inputLength = userInput.length;
-
-
+  
+  
   //Splits input into an array.
-
+  
   var inputSplit = userInput.split("");
-
-//Code using Evan's idea and his fix.
-
+  
+  //Code using Evan's idea and his fix.
+  
   alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
- for (il = 0; il < inputLength; il++) {
-
+  
+  for (il = 0; il < inputLength; il++) {
+    
     for (i = 0; i < 26; i++) {
-      // var i1 = i - 1
-      if (inputSplit[il] === alphabet[i]) { //-1
-        console.log(inputSplit[il]);
-        console.log(i);
+      
+      if (inputSplit[il] === alphabet[i]) {
+        
+        //Debug code from Evan is commented out.
+        //console.log(inputSplit[il]);
+        //console.log(i);
+        
         var letter = Number(i) + Number(shiftNum);
         if (letter > 25) {
           var letter = letter - 26;
-          
         }
-        letterPos = alphabet[letter];
-        var Answer = Answer + letterPos;
+        
+        var Answer = Answer + alphabet[letter];
         break;
       }
-//alert();
     }
-
-    //Combines  letters
-
   }
-
+  
   //Show result
   alert("Your secret message is " +  Answer);
   document.getElementById("Answer").innerHTML = "Your secret message is " + Answer;
-
+  
 }
